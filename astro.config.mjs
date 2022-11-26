c5b3d288daf6ed9d904a17817a7802c0da1db9b6
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-11-25 20:20:45 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // Configure more options from https://docs.astro.build/en/reference/configuration-reference
 import { defineConfig } from 'astro/config'
 
-export default defineConfig(
+export default defineConfig({
   site: 'https://example.com', // The URL of your deployed production site
 })
